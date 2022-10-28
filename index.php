fa8cb6c5fed6e75c5d735ad7a702edac722f759b
Bagus Alvin <bagusalvin224@gmail.com>
2022-10-28 01:36:35 -0700

    Commit Kedua

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Belajar Git - Belajar</title>
+    </head>
+    <body>
+        <p>Hallo Semua, Saya sedang belajar Git</p>
+    </body>
+</html>
